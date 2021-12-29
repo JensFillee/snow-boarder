95fca99c079bdfa9ccbfc7fc13fabac3bf3ab68f
JensFillee <r0800028@student.thomasmore.be>
2021-12-29 13:48:41 +0100

    reload scene when player wins/loses

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 0.5f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
-            Debug.Log("You bonked your head!");
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        // Load "level1" scene (has sceneIndex 0 (see Build Settings))
+        SceneManager.LoadScene(0);
     }
 }
