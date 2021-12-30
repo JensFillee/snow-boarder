097471e3a2da8b168382686adde50a9a2668ae26
JensFillee <r0800028@student.thomasmore.be>
2021-12-30 11:53:53 +0100

    triggering particle effects

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float reloadDelay = 0.5f;
+    [SerializeField] ParticleSystem finishEffect;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", reloadDelay);
         }
     }
