543fdcbcabd6e44f6dd9b16619b9212aa5772dc5
JensFillee <r0800028@student.thomasmore.be>
2022-01-15 09:15:58 +0100

    added sound effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,11 +7,15 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float reloadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
             crashEffect.Play();
+            // Play a specific audioclip (=> so you can have multiple SFX on same gameobject)
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", reloadDelay);
         }
     }
